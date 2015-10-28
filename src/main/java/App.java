2d654b510c2e92fc27fef64bc8a355af0b98bcdc
muraiki <muraiki@users.noreply.github.com>
2015-10-28 07:56:27 -0400

    use a layout. now on lesson https://www.learnhowtoprogram.com/lessons/using-forms-with-spark

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,9 +8,19 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
+        String layout = "templates/layout.vsl";
 
         get("/", (request, response) -> {
-           return new ModelAndView(new HashMap(), "templates/hello.vsl");
+            HashMap model = new HashMap();
+            model.put("template", "templates/hello.vsl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/favorite_photos", (request, response) -> {
+            HashMap model = new HashMap();
+            model.put("template", "templates/favorite_photos.vsl");
+
+            return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
     }
 }
